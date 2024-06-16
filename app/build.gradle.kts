bedcdfeb42a9d169d23c795b4ed2161d9ac2c3d2
SassyCowxx <7ukich@gmail.com>
2024-06-16 06:35:30 +0200

    Powiadomienia zrobione wg tutoriala, ale nie działają

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.bp_buddy"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/example/bp_buddy/MainActivity.kt
+++ app/src/main/java/com/example/bp_buddy/MainActivity.kt
@@ -1,10 +1,25 @@
 
 package com.example.bp_buddy
+import android.annotation.SuppressLint
+import android.app.AlarmManager
+import android.app.AlertDialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TimePicker
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.example.bp_buddy.databinding.ActivityMainBinding
+import com.example.bp_buddy.databinding.FragmentNotificationsBinding
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
@@ -12,11 +27,14 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.Calendar
 
 /**
  * Główna aktywność aplikacji, odpowiedzialna za zarządzanie widokami fragmentów oraz
  * interakcje użytkownika.
  */
+
+
 class MainActivity : AppCompatActivity() {
     /**
      * Metoda cyklu życia wywoływana podczas tworzenia aktywności.
@@ -26,8 +44,17 @@ class MainActivity : AppCompatActivity() {
      * @param savedInstanceState Zapisany stan aktywności, który może być użyty do przywrócenia
      *                           wcześniejszego stanu.
      */
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val binding = FragmentNotificationsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        createNotificationChannel()
+        binding.createNotification.setOnClickListener { scheduleNotification() }
+
         setContentView(R.layout.activity_main)
         val auth = FirebaseAuth.getInstance()
 
@@ -77,6 +104,68 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+
+    private fun scheduleNotification() {
+
+        val intent = Intent(applicationContext, Notification::class.java)
+        val title = "BP Buddy czeka!"
+        val message = "Pora na wprowadzenie kolejnego pomiaru."
+        intent.putExtra(messageExtra, message)
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            notificationID,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val time = getTime()
+        alarmManager.setAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, time, pendingIntent)
+
+        showAlert(time, title, message)
+    }
+
+    private fun showAlert(time: Long, title: String, message: String) {
+        val date = Date(time)
+        val dateFormat = android.text.format.DateFormat.getLongDateFormat(applicationContext)
+        val timeFormat = android.text.format.DateFormat.getTimeFormat(applicationContext)
+
+        AlertDialog.Builder(this)
+            .setTitle("Notification Scheduled")
+            .setMessage(
+                "Title: " + title +
+                "Message: " + message +
+                "At: " + dateFormat.format(date) + " " + timeFormat.format(date))
+            .setPositiveButton("Okay"){_,_ ->}
+            .show()
+
+    }
+
+    private fun getTime(): Long {
+        val binding = FragmentNotificationsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val minute = binding.timePicker.minute
+        val hour = binding.timePicker.hour
+        val day = binding.datePicker.dayOfMonth
+        val month = binding.datePicker.month
+        val year = binding.datePicker.year
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day, hour, minute)
+        return calendar.timeInMillis
+    }
+
+    private fun createNotificationChannel() {
+
+        val name = "Notif Channel"
+        val desc = "A Description of the Channel"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channelID, name, importance)
+        channel.description = desc
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 
     /**
