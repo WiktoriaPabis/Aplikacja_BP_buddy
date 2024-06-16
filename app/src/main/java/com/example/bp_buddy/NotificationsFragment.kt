7b75bb80ebea520deb877e371c6a366813964d2e
SassyCowxx <7ukich@gmail.com>
2024-06-16 14:49:20 +0200

    Działające powiadomienia, poprawiony layout

--- app/src/main/java/com/example/bp_buddy/NotificationsFragment.kt
+++ app/src/main/java/com/example/bp_buddy/NotificationsFragment.kt
@@ -2,17 +2,13 @@ package com.example.bp_buddy
 
 import android.app.AlarmManager
 import android.app.AlertDialog
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.bp_buddy.databinding.FragmentNotificationsBinding
 import java.util.*
@@ -27,6 +23,7 @@ class NotificationsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        binding.timePicker.setIs24HourView(true)
         return binding.root
     }
 
@@ -68,13 +65,13 @@ class NotificationsFragment : Fragment() {
         val timeFormat = android.text.format.DateFormat.getTimeFormat(requireContext())
 
         AlertDialog.Builder(requireContext())
-            .setTitle("Notification Scheduled")
+            .setTitle("Powiadomienie zaplanowane")
             .setMessage(
-                "Title: $title\n" +
-                        "Message: $message\n" +
-                        "At: ${dateFormat.format(date)} ${timeFormat.format(date)}"
+                "Tytuł: $title\n" +
+                        "Wiadomość: $message\n" +
+                        "Data i godzina: ${dateFormat.format(date)} ${timeFormat.format(date)}"
             )
-            .setPositiveButton("Okay") { _, _ -> }
+            .setPositiveButton("Potwierdź") { _, _ -> }
             .show()
     }
 
