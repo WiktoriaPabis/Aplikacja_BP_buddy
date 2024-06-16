c955823095d4bb09ea589e467f6a0e80cd5096f4
SassyCowxx <7ukich@gmail.com>
2024-06-17 00:25:51 +0200

    Skończony design historii wpisów

--- app/src/main/java/com/example/bp_buddy/MeasurementAdapter.kt
+++ app/src/main/java/com/example/bp_buddy/MeasurementAdapter.kt
@@ -3,6 +3,7 @@ package com.example.bp_buddy
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -27,7 +28,7 @@ class MeasurementsAdapter(measurementsList1: List<Map<String, Any>>, clickListen
      * @return Nowy ViewHolder stworzony na podstawie widoku elementu listy.
      */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MeasurementViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.measurement_adapter, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.entry_card_design, parent, false)
         return MeasurementViewHolder(view)
     }
 
@@ -60,15 +61,58 @@ class MeasurementsAdapter(measurementsList1: List<Map<String, Any>>, clickListen
      * @param itemView Widok reprezentujący pojedynczy element listy pomiarów.
      */
     class MeasurementViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val measurementTextView: TextView = itemView.findViewById(R.id.measurementTextView)
-
+        private val statSKR : TextView = itemView.findViewById(R.id.cisnienie_SKR)
+        private val statROZ : TextView = itemView.findViewById(R.id.cisnienie_ROZ)
+        private val statTETNO : TextView = itemView.findViewById(R.id.tetno)
+        private val mood : ImageView = itemView.findViewById(R.id.mood)
+        private val dateAndTime : TextView = itemView.findViewById(R.id.date_and_time)
+        private val entryNumber : TextView = itemView.findViewById(R.id.entry_number)
         /**
          * Metoda do wiązania danych pomiaru z widokiem ViewHoldera.
          *
          * @param measurement Mapa zawierająca dane pomiaru.
          */
         fun bind(measurement: Map<String, Any>) {
-            measurementTextView.text = measurement.toString()
+            statSKR.text = buildString {
+                append("Ciśnienie skurczowe: ")
+                append(measurement["Ciśnienie skurczowe"].toString())
+            }
+            statROZ.text = buildString {
+                append("Ciśnienie rozkurczowe: ")
+                append(measurement["Ciśnienie rozkurczowe"].toString())
+            }
+            statTETNO.text = buildString {
+                append("Tętno: ")
+                append(measurement["Tętno"].toString())
+            }
+                when (measurement["Samopoczucie"].toString()) {
+                    "1" -> mood.setBackgroundResource(R.drawable.buzki5)
+                    "2" -> mood.setBackgroundResource(R.drawable.buzki4)
+                    "3" -> mood.setBackgroundResource(R.drawable.buzki3)
+                    "4" -> mood.setBackgroundResource(R.drawable.buzki2)
+                    "5" -> mood.setBackgroundResource(R.drawable.buzki1)
+                }
+            dateAndTime.text = buildString {
+                append("Data i czas: ")
+                append(measurement["Data i czas"].toString().elementAt(8))
+                append(measurement["Data i czas"].toString().elementAt(9))
+                append(".")
+                append(measurement["Data i czas"].toString().elementAt(5))
+                append(measurement["Data i czas"].toString().elementAt(6))
+                append(".")
+                append(measurement["Data i czas"].toString().elementAt(0))
+                append(measurement["Data i czas"].toString().elementAt(1))
+                append(measurement["Data i czas"].toString().elementAt(2))
+                append(measurement["Data i czas"].toString().elementAt(3))
+                append(" ")
+                append(measurement["Data i czas"].toString().elementAt(11))
+                append(measurement["Data i czas"].toString().elementAt(12))
+                append(":")
+                append(measurement["Data i czas"].toString().elementAt(14))
+                append(measurement["Data i czas"].toString().elementAt(15))
+            }
+            entryNumber.text = measurement["docId"].toString()
+
         }
     }
 }
